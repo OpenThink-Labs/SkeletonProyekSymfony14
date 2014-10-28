28796a25a272694fe7d2d2e851dd22f7f6c18b9e
Wildan Maulana <wildan.m@openthinklabs.com>
2014-10-28 10:16:08 +0700

    Update, ditambah datatables

---
+++ apps/frontend/config/frontendConfiguration.class.php
@@ -0,0 +1,8 @@
+<?php
+
+class skeletonConfiguration extends sfApplicationConfiguration
+{
+  public function configure()
+  {
+  }
+}
--- apps/penjualan/config/penjualanConfiguration.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class penjualanConfiguration extends sfApplicationConfiguration
-{
-  public function configure()
-  {
-  }
-}
